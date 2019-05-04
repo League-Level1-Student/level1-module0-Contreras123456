804719f9e77ce773618663dc39e1129562955d8d
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-05-04 12:59:41 -0700

    did more hard work

--- src/SoundEffectsMachine.java
+++ src/SoundEffectsMachine.java
@@ -1,3 +1,7 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -6,18 +10,30 @@ public class SoundEffectsMachine {
 	public static void main(String[] args) {
 		new SoundEffectsMachine().createUI();
 	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JButton buttonjr = new JButton();
+	JButton buttonthethird = new JButton();
 	public void createUI() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JButton button = new JButton();
-		JButton buttonjr = new JButton();
-		JButton buttonthethird = new JButton();
 		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(button);
 		panel.add(buttonjr);
 		panel.add(buttonthethird);
 		frame.pack();
+		button.setText("sound effect 1");
+		buttonjr.setText("sound effect 2");
+		buttonthethird.setText("sound effect 3");
+		frame.pack();
+		playSound("soundeffect1.wav");
 	}
 
-}
+	
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	    	 sound.play();
+		}  	   	
+	}
+
+
